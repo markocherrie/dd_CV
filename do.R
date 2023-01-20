6e054c1f14acc27dea35a8c183ebf1481aae7284
Mark <mark.cherrie.mc@gmail.com>
2023-01-20 12:05:56 +0000

    update for HSE prop

--- do.R
+++ do.R
@@ -1,13 +1,13 @@
 # followed this https://annekelincoln.com/resume-in-r/
 
-remotes::install_local("package/datadrivencv-master.zip")
+#remotes::install_local("package/datadrivencv-master.zip")
 
-remotes::install_github("mitchelloharawild/icons")
+#remotes::install_github("mitchelloharawild/icons")
 
 datadrivencv::use_datadriven_cv(
-  full_name = "Mark Cherrie",
+  full_name = "Dr. Mark Cherrie",
   data_location = "https://docs.google.com/spreadsheets/d/1_JzRbY3aTBWV9ekf_Xb0CsHhn4Bz3mijG5efFz8MpG8/edit?usp=sharing",
-  pdf_location = "markcherrie.com",
-  html_location = "markcherrie.com",
+  pdf_location = "www.markcherrie.com",
+  html_location = "www.markcherrie.com",
   source_location = "https://github.com/markocherrie/dd_cv"
 )
